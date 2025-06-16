39593b1d68305c9fb3f182d4f99cafa07e5b3430
martinrocket <martin.ruble@gmail.com>
2025-06-16 13:34:45 -0600

    starting over with a log and absolute value

--- math_01.go
+++ math_01.go
@@ -6,10 +6,9 @@ import (
 )
 
 func main() {
-	fmt.Println("hello there")
-	fmt.Println(math.Pi)
-	fmt.Println(math.Cos(60))
-	fmt.Println(math.Sin(60))
-	fmt.Println("yes")
-	fmt.Println(math.Pow(5.0, 2.0))
+	for i := -5; i <= 5; i++ {
+		fmt.Print(i, " ")
+		fmt.Println(math.Abs(float64(i)))
+	}
+
 }
