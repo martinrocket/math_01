41abce3f15ffa2c236960b4602465c4c1fd58574
Martin Ruble <martin.ruble@gmail.com>
2025-06-16 12:20:09 -0600

    delete sin

--- math_01.go
+++ math_01.go
@@ -1,14 +1,13 @@
 package main
 
 import (
-    "fmt"
-    "math"
-  )
+	"fmt"
+	"math"
+)
 
-func main(){
-  fmt.Println("hello there")
-    fmt.Println(math.Pi)
-    fmt.Println(math.Cos(60))
-    fmt.Println(math.Sin(60))
-    fmt.Println(math.Sin(30))
-  }
+func main() {
+	fmt.Println("hello there")
+	fmt.Println(math.Pi)
+	fmt.Println(math.Cos(60))
+	fmt.Println(math.Sin(60))
+}
