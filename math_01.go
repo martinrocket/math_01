3e3ad28e6d021a7c93b5008f8ef415d33b1f2983
martinrocket <martin.ruble@gmail.com>
2025-06-16 13:51:55 -0600

    added function Absolute and Square Root

--- math_01.go
+++ math_01.go
@@ -5,13 +5,19 @@ import (
 	"math"
 )
 
+func Absandsqrt(x int) float64 {
+	return math.Pow(math.Abs(float64(x)), 2)
+}
+
 func main() {
 	/* Starting out with a loop that goes from neg 5 to
 	positive 5 then does a math.Abs (absolute value)
 	calculation to i*/
+
 	for i := -5; i <= 5; i++ {
 		fmt.Print(i, " ")
-		fmt.Println(math.Abs(float64(i)))
+		fmt.Print(math.Abs(float64(i)), " ")
+		fmt.Println(Absandsqrt(i))
 	}
 
 }
