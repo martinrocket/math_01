8be3a85d5e94be0e714a435778d819f52592ad01
martinrocket <martin.ruble@gmail.com>
2025-06-16 13:36:39 -0600

    starting over with a log and absolute value

--- math_01.go
+++ math_01.go
@@ -6,6 +6,9 @@ import (
 )
 
 func main() {
+	/* Starting out with a loop that goes from neg 5 to
+	positive 5 then does a math.Abs (absolute value)
+	calculation to i*/
 	for i := -5; i <= 5; i++ {
 		fmt.Print(i, " ")
 		fmt.Println(math.Abs(float64(i)))
